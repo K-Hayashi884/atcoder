9d897a8848c59a624190237dba0604677a8b6371
K-Hayashi884 <kakeru_hayashi@demia.co.jp>
2022-10-28 11:08:04 +0900

    abc274-d

--- abc274/abc274_d.py
+++ abc274/abc274_d.py
@@ -1,58 +1,30 @@
 n, x, y = list(map(int, input().split()))
 a = list(map(int, input().split()))
 
-lo = [0]*10
-le = [0]*10
-for index, i in enumerate(a[1:]):
+dp_x = [False] * (2*n*10+1)
+dp_x[n*10] = True
+dp_y = [False] * (2*n*10+1)
+dp_y[n*10] = True
+
+for index, ai in enumerate(a):
     if index%2 == 0:
-        le[i-1] += 1
+        dp_x_tmp = [False] * (2*n*10+1)
+        for i in range(2*n*10+1):
+            if dp_x[i]:
+                dp_x_tmp[i+ai] = True
+                if index > 1:
+                    dp_x_tmp[i-ai] = True
+        dp_x = dp_x_tmp
     else:
-        lo[i-1] += 1
+        dp_y_tmp = [False] * (2*n*10+1)
+        for i in range(2*n*10+1):
+            if dp_y[i]:
+                dp_y_tmp[i+ai] = True
+                dp_y_tmp[i-ai] = True
+        dp_y = dp_y_tmp
 
-def dfs_e(s, d):
-    if le[d-1] == 0:
-        if d == 10:
-            return s == y
-        else:
-            return dfs_e(s, d+1)
-    else:
-        if d == 10:
-            for i in range(le[9]):
-                if s+(10*i)+((-10)*(le[9]-i)) == y:
-                    return True
-                else:
-                    return False
-        else:
-            for i in range(le[d-1]+1):
-                if dfs_e(s+(d*i)+((-d)*(le[d-1]-i)), d+1):
-                    return True
-            return False
-
-def dfs_o(s, d):
-    if lo[d-1] == 0:
-        if d == 10:
-            return s == x
-        else:
-            return dfs_o(s, d+1)
-    else:
-        if d == 10:
-            for i in range(lo[9]):
-                if s+(10*i)+((-10)*(lo[9]-i)) == x:
-                    return True
-                else:
-                    return False
-        else:
-            for i in range(lo[d-1]+1):
-                if dfs_o(s+(d*i)+((-d)*(lo[d-1]-i)), d+1):
-                    return True
-            return False
-
-flg_e = dfs_e(0, 1)
-if flg_e:
-    if dfs_o(a[0], 1):
-        print("Yes")
-    else:
-        print("No")
+if dp_x[n*10+x] and dp_y[n*10+y]:
+    print('Yes')
 else:
-    print("No")
-
+    print('No')
+            
