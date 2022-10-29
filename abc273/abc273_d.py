afa40f476ae4df5e2711188a9f512cd81958a86b
K-Hayashi884 <kakeru_hayashi@demia.co.jp>
2022-10-29 15:38:50 +0900

    abc273-d

--- abc273/abc273_d.py
+++ abc273/abc273_d.py
@@ -1,65 +1,39 @@
-import time
+import bisect
+from collections import defaultdict
 
 h, w, rs, cs = list(map(int, input().split()))
 n = int(input())
-wall_r = [[0, h+1] for _ in range(w)]
-wall_c = [[0, w+1] for _ in range(h)]
+wall_r = defaultdict(lambda: [0, h+1])
+wall_c = defaultdict(lambda: [0, w+1])
 
 for i in range(n):
     r, c = list(map(int, input().split()))
-    wall_r[c-1].append(r)
-    wall_c[r-1].append(c)
+    wall_r[c].append(r)
+    wall_c[r].append(c)
 
-for wall in wall_r:
-    wall.sort()
-for wall in wall_c:
-    wall.sort()
+for k in wall_r.keys():
+    wall_r[k].sort()
+for k in wall_c.keys():
+    wall_c[k].sort()
+
+# print(wall_r)
+# print(wall_c)
 
 q = int(input())
-
-def lower_bound(l, t):
-    i = 0
-    j = len(l)-1
-    while i != j:
-        if j-i == 1 and l[i] < t and l[j] > t:
-            return l[i]
-        mid = int((i+j)/2)
-        if l[mid] == t:
-            return l[mid-1]
-        elif l[mid] > t:
-            j = mid
-        else:
-            i = mid
-    return l[i]
-    
-def upper_bound(l, t):
-    i = 0
-    j = len(l)-1
-    while i != j:
-        if j-i == 1 and l[i] < t and l[j] > t:
-            return l[j]
-        mid = int((i+j)/2)
-        if l[mid] == t:
-            return l[mid+1]
-        elif l[mid] > t:
-            j = mid
-        else:
-            i = mid
-    return l[i]
 
 for i in range(q):
     d, l = input().split()
     l = int(l)
     if d == "U":
-        w = lower_bound(wall_r[cs-1], rs)
-        rs = rs-l if rs-l>w else w+1
+        wall = wall_r[cs][bisect.bisect_right(wall_r[cs], rs)-1]
+        rs = rs-l if rs-l>wall else wall+1
     elif d == "D":
-        w = upper_bound(wall_r[cs-1], rs)
-        rs = rs+l if rs+l<w else w-1
+        wall = wall_r[cs][bisect.bisect_left(wall_r[cs], rs)]
+        rs = rs+l if rs+l<wall else wall-1
     elif d == "R":
-        w = upper_bound(wall_c[rs-1], cs)
-        cs = cs+l if cs+l<w else w-1
+        wall = wall_c[rs][bisect.bisect_left(wall_c[rs], cs)]
+        cs = cs+l if cs+l<wall else wall-1
     elif d == "L":
-        w = lower_bound(wall_c[rs-1], cs)
-        cs = cs-l if cs-l>w else w+1
+        wall = wall_c[rs][bisect.bisect_right(wall_c[rs], cs)-1]
+        cs = cs-l if cs-l>wall else wall+1
     print(rs, cs)
