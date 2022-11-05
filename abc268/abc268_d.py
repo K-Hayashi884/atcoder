a1d33bff7b93e8f53ec466b0095b253acf7b3193
K-Hayashi884 <kakeru_hayashi@demia.co.jp>
2022-11-06 02:38:19 +0900

    abc268-d

--- abc268/abc268_d.py
+++ abc268/abc268_d.py
@@ -1,12 +1,27 @@
-from collections import defaultdict
+def dfs(t, l):
+    if len(l) == 1:
+        return l[0] == t
+    elif len(l) == 0:
+        return False
+    mid = int(len(l)/2)
+    if l[mid] == t:
+        return True
+    elif l[mid] < t:
+        return dfs(t, l[mid+1:])
+    else:
+        return dfs(t, l[:mid])
+
+from itertools import permutations, product
+import sys
+sys.setrecursionlimit(10 ** 9)
 
 n, m = list(map(int, input().split()))
 s = []
 t = []
 for _ in range(n):
-    s.append(int(input()))
+    s.append(input())
 for _ in range(m):
-    t.append(int(input()))
+    t.append(input())
 
 if n == 1:
     if s[0] in t:
@@ -23,24 +38,53 @@ if n_underbar < 0:
     print(-1)
     exit()
 
-class recursivedefaultdict(defaultdict):
-    def __init__(self):
-        self.default_factory = type(self)
+t_int = []
 
-d = recursivedefaultdict()
 for e in t:
-    i = 0
-    tmp = d
+    tmp = ""
+    flg = True
     while len(e) > 0:
-        while i < len(e) and e[i+1] != "_":
+        i = 0
+        while i < len(e)-1 and e[i+1] != "_":
             i += 1
         if e[:i+1] in s:
-            tmp = tmp[e[:i+1]]
+            tmp += str(s.index(e[:i+1])+1)
             e = e[i+1:]
+        else:
+            flg = False
+            break
         if len(e) > 0:
             i = 0
             while e[i+1] == "_":
                 i += 1
-            tmp = tmp[i+1]
+            tmp += str(i+1)
             e = e[i+1:]
+    if flg:
+        t_int.append(int(tmp))
+t_int.sort()
+
+s_indexes = list(permutations([str(i) for i in range(1, n+1)]))
+underbar_indexes = list(product(range(n), repeat=n_underbar))
+for i in range(len(s_indexes)):
+    for j in range(len(underbar_indexes)):
+        t = ""
+        s_index = s_indexes[i]
+        underbar_index = [1]*(n-1)
+        for index in underbar_indexes[j]:
+            if index < n-1:
+                underbar_index[index] += 1
+        for k in range(n-1):
+            t += s_index[k]
+            t += str(underbar_index[k])
+        t += s_index[len(s_index)-1]
+        if not dfs(int(t), t_int):
+            ans = ""
+            for k in range(n-1):
+                ans += s[int(s_index[k])-1]
+                ans += "_" * underbar_index[k]
+            ans += s[int(s_index[len(s_index)-1])-1]
+            print(ans)
+            exit()
+
+print(-1)
 
