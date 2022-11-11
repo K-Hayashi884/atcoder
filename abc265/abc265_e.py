153c77089aef2d02c39c6b52279288cc68ce79a8
K-Hayashi884 <kakeru_hayashi@demia.co.jp>
2022-11-11 19:45:32 +0900

    abc265-e

--- abc265/abc265_e.py
+++ abc265/abc265_e.py
@@ -1,10 +1,31 @@
-import numpy as np
 n, m = list(map(int, input().split()))
 a, b, c, d, e, f = list(map(int, input().split()))
-dp = np.zeros([n, 2*10**9+1, 2*10**9+1])
+obs = set()
 for _ in range(m):
-    obstacle = list(map(int, input().split()))
-    for i in range(n):
-        dp[i][obstacle[0]][obstacle[1]] = -1
+    ob = input().split()
+    obs.add(ob[0]+","+ob[1])
 
-for i in range(n)
+ans = 3**n
+from collections import defaultdict
+dp = [defaultdict(lambda: defaultdict(lambda: 0)) for _ in range(n+1)]
+dp[0][0][0] += 1
+for i in range(1, n+1):
+    history = dp[i-1]
+    for x, ys in history.items():
+        for y, cnt in ys.items():
+            if str(x+a)+","+str(y+b) in obs:
+                ans -= 3**(n-i)
+            else:
+                dp[i][x+a][y+b] += cnt
+
+            if str(x+c)+","+str(y+d) in obs:
+                ans -= 3**(n-i)
+            else:
+                dp[i][x+c][y+d] += cnt
+
+            if str(x+e)+","+str(y+f) in obs:
+                ans -= 3**(n-i)
+            else:
+                dp[i][x+e][y+f] += cnt
+
+print(ans%998244353)
